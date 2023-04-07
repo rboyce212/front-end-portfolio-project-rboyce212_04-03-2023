a198494cc405c16a489c0de9216d76b23bfc1fac
rboyce212 <richboyce@pursuit.org>
2023-04-07 18:58:06 -0400

    few more minor edits

--- main.js
+++ main.js
@@ -57,7 +57,7 @@ function loadMovieDetails() {
       searchList.classList.add("hide-search-list");
       movieSearchBox.value = "";
       const result = await fetch(
-        `http://www.omdbapi.com/?i=${movie.dataset.id}&apikey=7f245b75`
+        `https://www.omdbapi.com/?i=${movie.dataset.id}&apikey=7f245b75`
       );
       const movieDetails = await result.json();
       // console.log(movieDetails);
